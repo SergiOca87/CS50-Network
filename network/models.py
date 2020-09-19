02080b365bbaac12892f2a30a573f1896b6f3483
ocasergi@gmail.com <ocasergi@gmail.com>
2020-09-19 16:56:45 +0200

    first commit, new post route

---
+++ network/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+
+# Posts
+class Post(models.Model):
+    author = models.ForeignKey(User, related_name="author", on_delete=models.CASCADE)
+    post_text = models.CharField(max_length=280)
+
+# Likes
+
+# Followers
+
+# python manage.py makemigrations and then python manage.py
